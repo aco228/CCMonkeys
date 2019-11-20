36f0c594d3e95ad1283263eb152e64271aa3c499
aco228 <aleksandar.k03@gmail.com>
2019-11-20 22:55:46 +0100

    minor

---
+++ _rest/sql/providers.sql
@@ -0,0 +1,14 @@
+/*
+-- Query: SELECT * FROM ccmonkeys.tm_provider
+LIMIT 0, 1000
+
+-- Date: 2019-11-20 17:30
+*/
+INSERT INTO `` (`providerid`,`identifier`,`name`,`price`,`description`) VALUES (1,'cc1','dogbain1',16,'{\"flow\": \" 3 step, p in frame\", \"redirect\": false, \"iframeUrls\": [\"https://joinsafelyonline.com/routes/?ofid=398&autoun=1&autopw=1&a_aid=ita4&a_bid=9fe6b1cc&j2p=1\"]}');
+INSERT INTO `` (`providerid`,`identifier`,`name`,`price`,`description`) VALUES (2,'cc2','dogbain2',20,'{\"flow\": \" 3 step, p in frame\", \"redirect\": false, \"iframeUrls\": [\"https://joinsafelyonline.com/routes/?ofid=398&autoun=1&autopw=1&wlid=dbc2&a_aid=ita4cpa&a_bid=f27b7bf9&j2p=1\"]}');
+INSERT INTO `` (`providerid`,`identifier`,`name`,`price`,`description`) VALUES (3,'cc3','invoreak',20,'{\"flow\": \" 3 step, p in frame\", \"redirect\": false, \"iframeUrls\": [\"https://cc.inveroak.co.uk/bookings/couponmaster.aspx?\"]}');
+INSERT INTO `` (`providerid`,`identifier`,`name`,`price`,`description`) VALUES (4,'cc4','cccash',25,'{\"flow\": \"away\", \"redirect\": true }');
+INSERT INTO `` (`providerid`,`identifier`,`name`,`price`,`description`) VALUES (5,'cc5','cloverdale',20,'{\"flow\": \"away\", \"redirect\": true }');
+INSERT INTO `` (`providerid`,`identifier`,`name`,`price`,`description`) VALUES (6,'cc6','vipresponse',25,'{\"flow\": \" 3 step, p in frame\", \"redirect\": false, \"iframeUrls\": [\"AU: https://adltclck.com/clickout/863819/3806601/?\", \"ES: https://adltclck.com/clickout/873998/3815590/?\", \"UK: https://t.viprsp.nl/clickout/908668/3820868?\", \"IT: https://adltclck.com/clickout/873993/3859778?\"]}');
+INSERT INTO `` (`providerid`,`identifier`,`name`,`price`,`description`) VALUES (7,'cc7','thirdpartyclick2sms_',1,'{\"flow\": \" 3 step, c2sms\", \"redirect\": false }');
+INSERT INTO `` (`providerid`,`identifier`,`name`,`price`,`description`) VALUES (8,'test','updateFromApi',20,'inserted with api');
